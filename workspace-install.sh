5cd025d44a91938c850b4e1b2c052c8f8e480ece
danilapog <danil.titarenko@onlyoffice.com>
2023-03-24 18:19:46 +0300

    Fix getting script exit codes
    Use set -e parameter for correct work with 0 command

--- workspace-install.sh
+++ workspace-install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # (c) Copyright Ascensio System Limited 2010-2021
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
