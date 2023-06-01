a9473f6948342d413ef33a5d70f4c37e9e7a0320
Evgeniy Antonyuk <antonyuk.evgenyiy@onlyoffice.com>
2023-06-01 19:47:22 +0500

    Fix script interruption

--- install-Debian/tools.sh
+++ install-Debian/tools.sh
@@ -113,4 +113,4 @@ DIST=`echo "$DIST" | tr '[:upper:]' '[:lower:]' | xargs`;
 DISTRIB_CODENAME=`echo "$DISTRIB_CODENAME" | tr '[:upper:]' '[:lower:]' | xargs`;
 REV=`echo "$REV" | xargs`;
 
-[ "$DISTRIB_CODENAME" = "kinetic" ] || [ "$DISTRIB_CODENAME" = "lunar" ] && DISTRIB_CODENAME="jammy" && REV="22.04"
+[ "$DISTRIB_CODENAME" = "kinetic" ] || [ "$DISTRIB_CODENAME" = "lunar" ] && { DISTRIB_CODENAME="jammy"; REV="22.04"; } || true
