f3dd94c99436209305d066cf8bce5dfabbd51fac
Evgeniy Antonyuk <antonyuk.evgenyiy@onlyoffice.com>
2023-06-26 18:54:30 +0500

    fix Bug 62854 - Add the use of the default authentication plugin for mysql (#38)
    * Change default mysql authentication plugin to caching_sha2_password
    
    * Add the use of default authentication plugin for mysql

--- install-RedHat/install-app.sh
+++ install-RedHat/install-app.sh
@@ -101,9 +101,11 @@ if [ "${MYSQL_FIRST_TIME_INSTALL}" = "true" ]; then
 		   MYSQL="mysql --connect-expired-password -u$MYSQL_SERVER_USER -p${MYSQL_TEMPORARY_ROOT_PASS} -D mysql";
 		fi
 
-		   $MYSQL -e "ALTER USER '${MYSQL_SERVER_USER}'@'localhost' IDENTIFIED WITH mysql_native_password BY '${MYSQL_SERVER_PASS}'" >/dev/null 2>&1 \
-		   || $MYSQL -e "UPDATE user SET plugin='mysql_native_password', authentication_string=PASSWORD('${MYSQL_SERVER_PASS}') WHERE user='${MYSQL_SERVER_USER}' and host='localhost';"		
+		MYSQL_AUTHENTICATION_PLUGIN=$($MYSQL -e "SHOW VARIABLES LIKE 'default_authentication_plugin';" -s | awk '{print $2}')
+		MYSQL_AUTHENTICATION_PLUGIN=${MYSQL_AUTHENTICATION_PLUGIN:-caching_sha2_password}
 
+		$MYSQL -e "ALTER USER '${MYSQL_SERVER_USER}'@'localhost' IDENTIFIED WITH ${MYSQL_AUTHENTICATION_PLUGIN} BY '${MYSQL_SERVER_PASS}'" >/dev/null 2>&1 \
+		|| $MYSQL -e "UPDATE user SET plugin='${MYSQL_AUTHENTICATION_PLUGIN}', authentication_string=PASSWORD('${MYSQL_SERVER_PASS}') WHERE user='${MYSQL_SERVER_USER}' and host='localhost';"
 
 		systemctl restart mysqld
 	fi
