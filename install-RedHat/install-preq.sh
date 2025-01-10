b6f2f2669ed0d2007934393637b40d5b790900f9
Vladimir Ischenko <vladimir.ischenko@onlyoffice.com>
2025-01-10 13:29:43 +0000

    Fix bug 71592 – Replace trust auth with secure method (#4)
    Reviewed-on: https://git.onlyoffice.com/ONLYOFFICE/OneClickInstall-Workspace/pulls/4
    Reviewed-by: Evgeniy Antonyuk <antonyuk.evgenyiy@onlyoffice.com>
    Co-authored-by: Vladimir Ischenko <vladimir.ischenko@onlyoffice.com>
    Co-committed-by: Vladimir Ischenko <vladimir.ischenko@onlyoffice.com>

--- install-RedHat/install-preq.sh
+++ install-RedHat/install-preq.sh
@@ -259,6 +259,9 @@ fi
 
 postgresql-setup initdb	|| true
 
+sed -E -i "s/(host\s+(all|replication)\s+all\s+(127\.0\.0\.1\/32|\:\:1\/128)\s+)(ident|trust|md5)/\1scram-sha-256/" /var/lib/pgsql/data/pg_hba.conf
+sed -i "s/^#\?password_encryption = .*/password_encryption = 'scram-sha-256'/" /var/lib/pgsql/data/postgresql.conf
+
 if [ -z $ELASTICSEARCH_REPOSITORY ]; then
 	rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 
