e5f73a47a13c21e58c459784b27e3972e10f2d8d
Evgeniy Antonyuk <antonyuk.evgenyiy@onlyoffice.com>
2025-12-23 12:00:45 +0200

    Add support for installing docker on RHEL

--- install.sh
+++ install.sh
@@ -931,13 +931,22 @@ install_docker () {
 		systemctl start docker
 		systemctl enable docker
 
-	elif [ "${DIST}" == "Red Hat Enterprise Linux Server" ]; then
+	elif [[ "${DIST}" == Red\ Hat\ Enterprise\ Linux* ]]; then
 
-		echo ""
-		echo "Your operating system does not allow Docker CE installation."
-		echo "You can install Docker EE using the manual here - https://docs.docker.com/engine/installation/linux/rhel/"
-		echo ""
-		exit 1;
+		if [[ "${REV}" -gt "7" ]]; then
+			yum remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine podman runc > null
+			yum install -y yum-utils
+			yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+			yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin
+			systemctl start docker
+			systemctl enable docker
+		else
+			echo ""
+			echo "Your operating system does not allow Docker CE installation."
+			echo "You can install Docker EE using the manual here - https://docs.docker.com/engine/installation/linux/rhel/"
+			echo ""
+			exit 1
+		fi
 
 	elif [ "${DIST}" == "SuSe" ]; then
 
